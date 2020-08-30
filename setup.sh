6b7392923f3a2a20e1cf0177a2319f477e666de7
Ryan Day <rddays@gmail.com>
2020-08-30 09:10:39 -0500

    Make setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+cp bash_profile ~/.bash_profile
+cp tmux.conf ~/.tmux.conf
+cp vimrc ~/.vimrc
+mkdir ~/.vim
+# install vim package manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp vim/coc-settings.json ~/.vim/coc-settings.json
+

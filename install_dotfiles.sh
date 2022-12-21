dda48074502877be746fcb06d3b9f3712d485208
Ryan Day <rddays@gmail.com>
2022-12-21 13:54:12 -0600

    Add force overwrite for symlinks

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -s $PWD/zshrc $HOME/.zshrc
-ln -s $PWD/zsh_functions.zsh $HOME/.zsh_functions.zsh
-ln -s $PWD/zsh_plugins.txt $HOME/.zsh_plugins.txt
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/alacritty.yml $HOME/.alacritty.yml
+ln -fs $PWD/zshrc $HOME/.zshrc
+ln -fs $PWD/zsh_functions.zsh $HOME/.zsh_functions.zsh
+ln -fs $PWD/zsh_plugins.txt $HOME/.zsh_plugins.txt
+ln -fs $PWD/tmux.conf $HOME/.tmux.conf
+ln -fs $PWD/vimrc $HOME/.vimrc
+ln -fs $PWD/alacritty.yml $HOME/.alacritty.yml
 # install vim package manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

10bde9f7a6a9e333e7498bfed7d04550eb8cd8aa
ryandday <ryan.day@mapbox.com>
2025-03-19 10:58:03 -0500

    Update for ubuntu

---
+++ install_ubuntu.sh
@@ -0,0 +1,12 @@
+
+
+sudo apt-get update --fix-missing
+sudo apt install zsh
+sudo apt install fzf
+sudo apt install ripgrep
+sudo apt install zsh-antidote
+sudo apt install lf
+sudo apt install kitty
+sudo apt install ccls
+sudo apt install plocate
+chsh -s $(which zsh)

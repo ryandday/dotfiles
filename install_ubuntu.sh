2a532f5a90c102728277a4aee9af59a2a7774c41
ryandday <ryan.day@mapbox.com>
2025-03-21 16:05:51 +0000

    Put all in same one

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,12 +1,4 @@
-
-
 sudo apt-get update --fix-missing
-sudo apt install zsh
-sudo apt install fzf
-sudo apt install ripgrep
-sudo apt install zsh-antidote
-sudo apt install lf
-sudo apt install kitty
-sudo apt install ccls
-sudo apt install plocate
+sudo apt install zsh fzf ripgrep zsh-antidote lf kitty ccls plocate build-essential
+
 chsh -s $(which zsh)

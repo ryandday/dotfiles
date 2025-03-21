091c1bb401e0fcd76b5d0bb19a30552dedcf2977
ryandday <ryan.day@mapbox.com>
2025-03-21 16:08:32 +0000

    Add some more

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,4 @@
 sudo apt-get update --fix-missing
-sudo apt install zsh fzf ripgrep zsh-antidote lf kitty ccls plocate build-essential
+sudo apt install zsh fzf ripgrep zsh-antidote lf kitty ccls plocate build-essential llvm clang-format clangd
 
 chsh -s $(which zsh)

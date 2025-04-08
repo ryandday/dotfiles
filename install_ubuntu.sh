0504ebbb0764d611e236ca0440b56959c346fe92
ryandday <ryan.day@mapbox.com>
2025-04-07 21:30:00 -0500

    Add sccache

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,4 @@
 sudo apt-get update --fix-missing
-sudo apt install zsh fzf ripgrep zsh-antidote lf kitty ccls plocate build-essential llvm clang-format clangd
+sudo apt install zsh fzf ripgrep zsh-antidote lf kitty ccls plocate build-essential llvm clang-format clangd sccache
 
 chsh -s $(which zsh)

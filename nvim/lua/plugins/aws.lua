0c55943e9d499a1cf3a38a410d8706a4600b9397
Ryan Day <ryan.david.day@gmail.com>
2025-05-30 15:35:03 -0500

    Load plugin

---
+++ nvim/lua/plugins/aws.lua
@@ -0,0 +1,59 @@
+return {
+  -- AWS resource explorer plugin
+  {
+    dir = vim.fn.expand("~/repos/dotfiles/nvim/lua/myplugins/aws-nvim"),
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons"
+    },
+    cmd = {
+      "AWSNvimOpen",
+      "AWSNvimStack",
+      "AWSNvimRefresh",
+      "AWSNvimFilter",
+      "AWSNvimProfile",
+      "AWSNvimRegion"
+    },
+    keys = {
+      { "<leader>aws", "<cmd>AWSNvimOpen<cr>", desc = "Open AWS Explorer" },
+      { "<leader>awf", "<cmd>AWSNvimFilter ", desc = "Filter AWS Resources" },
+      { "<leader>awr", "<cmd>AWSNvimRegion ", desc = "Change AWS Region" },
+      { "<leader>awp", "<cmd>AWSNvimProfile ", desc = "Change AWS Profile" },
+    },
+    opts = {
+      -- AWS settings
+      region = "us-east-1",
+      profile = "", -- Empty string will use default AWS profile
+      
+      -- Cache settings
+      cache_ttl = {
+        stack = 600,    -- 10 minutes
+        service = 300,  -- 5 minutes
+        task = 120,     -- 2 minutes
+        container = 60  -- 1 minute
+      },
+      
+      -- UI settings
+      split_direction = "right",
+      width = 40,
+      height = 20,
+      
+      -- Visual indicators
+      icons = {
+        expanded = "▼",
+        collapsed = "▶",
+        leaf = " ",
+        loading = "⟳",
+        status = {
+          ok = "✓",
+          warning = "⚠",
+          error = "✗",
+          unknown = "?"
+        }
+      }
+    },
+    config = function(_, opts)
+      require("aws-nvim").setup(opts)
+    end,
+  }
+}

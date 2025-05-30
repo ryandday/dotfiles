0686d20e4461bd98b08968f81fecbb0fc96b8b72
Ryan Day <ryan.david.day@gmail.com>
2025-05-30 12:11:53 -0500

    change to gruvbox nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,33 @@
 return {
   {
-    "morhetz/gruvbox",
+    "ellisonleao/gruvbox.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd("colorscheme gruvbox")
-      vim.opt.background = "dark"
+      require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+      vim.o.background = "dark" -- or "light" for light mode
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
 } 
